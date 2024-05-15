e6f22862c4564a1b62e19532f0d28a569fd95157
Amal T Scaria <amaltscaria22@gmail.com>
2024-05-15 22:09:48 +0530

    Add basics file for primitive types.

---
+++ Typescript/basics/basics.ts
@@ -0,0 +1,22 @@
+const add = (n1: number, n2: number, showResult: boolean, resultPhrase: string) => {
+  //Vanilla js
+  // if(typeof n1 === 'number' && typeof n2 === 'number')
+  // return n1 + n2;
+  // else throw new Error('Invalid Input!');
+  //without throwing the erro that is without breaking our app
+  //we could have used ts to prevent this during development itself.
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(resultPhrase + result);
+  } else {
+    return n1 + n2;
+  }
+};
+
+const number1 = 2.8;
+const number2 = 5;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+
+const result = add(number1, number2, printResult, resultPhrase);
