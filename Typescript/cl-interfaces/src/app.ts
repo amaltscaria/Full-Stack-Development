5b600e07da7bb8b3203500cdb0923cac4b97ce77
Amal T Scaria <amaltscaria22@gmail.com>
2024-05-17 14:44:02 +0530

    Add static methods and static properties.

--- Typescript/cl-interfaces/src/app.ts
+++ Typescript/cl-interfaces/src/app.ts
@@ -1,1 +1,19 @@
 //static methods
+// eg Math we can access it globally by just Math.pow()
+// we are not required to create a ojbect like new Math();
+// we call the static method  directly on the class without creating an object;
+// Threfore we use the class as the grouping mechanism;
+
+class StaticFunctions {
+    static firstName = 'Amal';
+    constructor() {
+        console.log(StaticFunctions.firstName);
+    }
+    static createEmployee(name: string) {
+        return {name}
+    }
+}
+
+console.log(StaticFunctions.firstName)
+const obj = StaticFunctions.createEmployee('Leo');
+console.log(obj);
